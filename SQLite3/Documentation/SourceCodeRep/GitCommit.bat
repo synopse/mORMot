f11d0035edd7b6923a60f0de40a70bd8e88c994d
Synopse <webcontact01@synopse.info>
2014-05-02 13:18:49 +0200

    fixed git commit script

--- SQLite3/Documentation/SourceCodeRep/GitCommit.bat
+++ SQLite3/Documentation/SourceCodeRep/GitCommit.bat
@@ -5,7 +5,7 @@
 @echo.
 
 ROBOCOPY %1 %2 /xf _fossil_  >nul
-ROBOCOPY %1\SQLite3 %2\SQLite3  >nul
+ROBOCOPY %1\SQLite3 %2\SQLite3 /s >nul
 ROBOCOPY %1\SynDBDataSet %2\SynDBDataSet  >nul
 
 %3 commit -a --file=%4
