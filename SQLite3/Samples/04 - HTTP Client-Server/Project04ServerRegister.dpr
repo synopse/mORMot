a58be5d381cbb53ee91bf41162b88846bcfa2f88
Synopse <webcontact01@synopse.info>
2014-05-26 08:55:00 +0200

    added Project04ServerRegister.dpr program, to make clear how to register an URI for http.sys at installation: it is not mandatory at all (and even not a good idea) to run the server instance with administrator rights!

---
+++ SQLite3/Samples/04 - HTTP Client-Server/Project04ServerRegister.dpr
@@ -0,0 +1,37 @@
+{{ in order to be able to use http.sys server for Project04Server.exe 
+ under Vista or Seven, call first this program with Administrator rights
+  - you can unregister it later with command line parameter /delete }
+program Project04ServerRegister;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SynCommons,
+  SynCrtSock,
+  SysUtils;
+
+const
+  REGSTR: array[boolean] of string = (
+    'Registration', 'Deletion');
+
+{$R VistaAdm.res} // force elevation to Administrator under Vista/Seven
+
+var delete: boolean;
+
+procedure Call(const Root: RawByteString);
+begin
+  writeln(REGSTR[delete],' of /',root,':8080/+ for http.sys');
+  writeln(THttpApiServer.AddUrlAuthorize(root,'8080',false,'+',delete));
+end;
+
+begin
+  // test if we have to un-register the url
+  delete := (ParamCount=1) and SameText(ParamStr(1),'/DELETE');
+  // perform url (un)registration for http.sys
+  // (e.g. to be run as administrator under Windows Vista/Seven)
+  Call('root');
+  // we're done
+  WriteLn('Done - Press ENTER to Exit');
+  ReadLn;
+end.
+ 
