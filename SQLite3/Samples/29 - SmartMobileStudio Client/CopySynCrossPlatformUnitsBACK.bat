5eb749880e9fdc11621452f6ea5ee085f2be0371
Synopse <webcontact01@synopse.info>
2014-06-28 11:42:09 +0200

    added CopySynCrossPlatformUnitsBACK.bat to synchronize "29 - SmartMobileStudio Client" units after work in shared units within the SMS IDE

---
+++ SQLite3/Samples/29 - SmartMobileStudio Client/CopySynCrossPlatformUnitsBACK.bat
@@ -0,0 +1,5 @@
+rem To be used to synchronize units modified within SMS IDE
+xcopy SynCrossPlatformSpecific.pas ..\..\..\CrossPlatform /Y
+xcopy SynCrossPlatformCrypto.pas ..\..\..\CrossPlatform /Y
+xcopy SynCrossPlatformREST.pas ..\..\..\CrossPlatform /Y
+pause
