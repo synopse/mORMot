9175099d9770904810d58823e1ba439c0c2b87df
Synopse <webcontact01@synopse.info>
2014-10-13 15:32:00 +0200

    added missing file for FPC (cross-platform) compilation under Linux

---
+++ SynFPCLinux.pas
@@ -0,0 +1,182 @@
+/// wrapper for Windows functions translated to Linux
+unit SynFPCLinux;
+
+{
+    This file is part of Synopse mORMot framework.
+
+    Synopse mORMot framework. Copyright (C) 2014 Arnaud Bouchez
+      Synopse Informatique - http://synopse.info
+
+  *** BEGIN LICENSE BLOCK *****
+  Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+  The contents of this file are subject to the Mozilla Public License Version
+  1.1 (the "License"); you may not use this file except in compliance with
+  the License. You may obtain a copy of the License at
+  http://www.mozilla.org/MPL
+
+  Software distributed under the License is distributed on an "AS IS" basis,
+  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+  for the specific language governing rights and limitations under the License.
+
+  The Original Code is Synopse mORMot framework.
+
+  The Initial Developer of the Original Code is Alfred Glaenzer.
+
+  Portions created by the Initial Developer are Copyright (C) 2014
+  the Initial Developer. All Rights Reserved.
+
+  Contributor(s):
+  - Arnaud Bouchez
+
+  
+  Alternatively, the contents of this file may be used under the terms of
+  either the GNU General Public License Version 2 or later (the "GPL"), or
+  the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+  in which case the provisions of the GPL or the LGPL are applicable instead
+  of those above. if you wish to allow use of your version of this file only
+  under the terms of either the GPL or the LGPL, and not to allow others to
+  use your version of this file under the terms of the MPL, indicate your
+  decision by deleting the provisions above and replace them with the notice
+  and other provisions required by the GPL or the LGPL. if you do not delete
+  the provisions above, a recipient may use your version of this file under
+  the terms of any one of the MPL, the GPL or the LGPL.
+
+  ***** END LICENSE BLOCK *****
+
+
+  Version 1.18
+  - initial revision
+
+}
+
+interface
+
+{$MODE objfpc}
+{$inline on}
+{$h+}
+
+uses
+  SysUtils
+  {$ifdef Linux}
+  ,UnixType
+  ,lazutf8sysutils
+  {$endif};
+
+const
+{ HRESULT codes, delphi-like }
+  NOERROR = 0;
+  NO_ERROR = 0;
+  INVALID_HANDLE_VALUE =  THandle(-1);
+
+/// compatibility function, wrapping Win32 API mutex initialization
+procedure InitializeCriticalSection(var cs : TRTLCriticalSection); inline;
+
+/// compatibility function, wrapping Win32 API mutex finalization
+procedure DeleteCriticalSection(var cs : TRTLCriticalSection); inline;
+
+{$ifdef Linux}
+
+/// compatibility function, wrapping Win32 API high resolution timer
+procedure QueryPerformanceCounter(var Value: Int64); inline;
+
+/// compatibility function, wrapping Win32 API high resolution timer
+function QueryPerformanceFrequency(var Value: Int64): boolean;
+
+/// compatibility function, wrapping Win32 API file position change
+function SetFilePointer(hFile: cInt; lDistanceToMove: TOff; lpDistanceToMoveHigh: Pointer; dwMoveMethod: cint): TOff; inline;
+
+/// compatibility function, wrapping Win32 API file truncate at current position
+procedure SetEndOfFile(hFile: cInt); inline;
+
+/// compatibility function, wrapping Win32 API last error code
+function GetLastError: longint; inline;
+
+/// compatibility function, wrapping Win32 API text comparison
+function CompareStringW(GetThreadLocale: DWORD; dwCmpFlags: DWORD; lpString1: Pwidechar;
+  cchCount1: longint; lpString2: Pwidechar; cchCount2: longint): longint; inline;
+
+/// returns the current UTC time
+function FPCNowUTC: TDateTime; inline;
+
+/// compatibility function, to be implemented according to the running OS
+// - expect more or less the same result as the homonymous Win32 API function
+function GetTickCount64: Int64; inline;
+
+{$endif Linux}
+
+
+implementation
+
+{$ifdef Linux}
+uses
+  Classes, Unix, BaseUnix, linux, dynlibs;
+{$endif}
+
+procedure InitializeCriticalSection(var cs : TRTLCriticalSection);
+begin
+  InitCriticalSection(cs);
+end;
+
+procedure DeleteCriticalSection(var cs : TRTLCriticalSection);
+begin
+  DoneCriticalSection(cs);
+end;
+
+{$ifdef Linux}
+procedure QueryPerformanceCounter(var Value: Int64);
+begin
+  clock_gettime(CLOCK_MONOTONIC,@Value);
+end;
+
+function QueryPerformanceFrequency(var Value: Int64):boolean;
+const
+  C_THOUSAND = 1000;
+  C_MILLION  = C_THOUSAND * C_THOUSAND;
+  C_BILLION  = C_THOUSAND * C_THOUSAND * C_THOUSAND;
+var
+  r : TTimeSpec;
+  FIsHighResolution : boolean;
+begin
+  FIsHighResolution := (clock_getres(CLOCK_MONOTONIC,@r) = 0);
+  FIsHighResolution := FIsHighResolution and (r.tv_nsec <> 0);
+  if (r.tv_nsec <> 0) then
+      value := C_BILLION div r.tv_nsec;
+  result := FIsHighResolution;
+end;
+
+function SetFilePointer(hFile: cInt; lDistanceToMove: TOff;
+  lpDistanceToMoveHigh: Pointer; dwMoveMethod: cint): TOff;
+begin
+  result := FpLseek( hFile, lDistanceToMove, SEEK_CUR );
+end;
+
+procedure SetEndOfFile(hFile: cInt);
+begin
+  FileSeek(hFile,0,soFromEnd);
+end;
+
+function GetLastError: longint;
+begin
+  result := GetLastOSError;
+end;
+
+function CompareStringW(GetThreadLocale: DWORD; dwCmpFlags: DWORD; lpString1: Pwidechar;
+  cchCount1: longint; lpString2: Pwidechar; cchCount2: longint): longint;
+begin
+  result := WideCompareText(Pwidechar(lpString1),Pwidechar(lpString2));
+end;
+
+function FPCNowUTC: TDateTime;
+begin
+  result := lazutf8sysutils.NowUTC;
+end;
+
+function GetTickCount64: Int64;
+begin
+  result := lazutf8sysutils.GetTickCount64;
+end;
+
+{$endif}
+
+end.
