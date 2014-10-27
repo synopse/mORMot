73db670514fadab0e1f479500996f1c8e5a99ebc
Synopse <webcontact01@synopse.info>
2014-10-27 20:43:14 +0100

    added specific MVCServerPostgreSQL.dpr for PostgreSQL version of "30 - MVC Server"

--- SQLite3/Samples/30 - MVC Server/MVCServer.dpr
+++ SQLite3/Samples/30 - MVC Server/MVCServer.dpr
@@ -3,16 +3,6 @@ program MVCServer;
 
 {$APPTYPE CONSOLE}
 
-
-{.$define USEZEOSPOSTGRESQL}
-{.$define USEFIREDACPOSTGRESQL}
-
-// direct ZDBC/FireDAC driver needs only libpq.dll and libintl.dll e.g. from
-// http://www.enterprisedb.com/products-services-training/pgbindownload
-
-{$ifdef USEZEOSPOSTGRESQL}    {$define USESYNDB} {$endif}
-{$ifdef USEFIREDACPOSTGRESQL} {$define USESYNDB} {$endif}
-
 uses
   {$I SynDprUses.inc}    // will enable FastMM4 prior to Delphi 2006
   SynCrtSock,
@@ -23,52 +13,21 @@ uses
   mORMotSQLite3,
   mORMotHttpServer,
   mORMotMVC,
-  {$ifdef USESYNDB}
-  SynDB,
-  mORMotDB,
-  {$ifdef USEZEOSPOSTGRESQL}
-  SynDBZeos,
-  {$endif}
-  {$ifdef USEFIREDACPOSTGRESQL}
-  SynDBFireDAC,
-  {$ifdef ISDELPHIXE5} FireDAC.Phys.PG, {$else} uADPhysPG, {$endif}
-  {$endif}
-  {$endif}
   MVCModel,
   MVCViewModel,
   SysUtils;
 
 var aModel: TSQLModel;
-    {$ifdef USESYNDB}
-    aExternalDB: TSQLDBConnectionProperties;
-    {$endif}
     aServer: TSQLRestServerDB;
     aApplication: TBlogApplication;
     aHTTPServer: TSQLHttpServer;
 begin
   aModel := CreateModel;
   try
-    {$ifdef USESYNDB}
-    {$ifdef USEZEOSPOSTGRESQL}
-    aExternalDB := TSQLDBZEOSConnectionProperties.Create(
-      TSQLDBZEOSConnectionProperties.URI(dPostgreSQL,'localhost:5432'),
-    {$endif}
-    {$ifdef USEFIREDACPOSTGRESQL}
-    aExternalDB := TSQLDBFireDACConnectionProperties.Create(
-      'PG?Server=localhost;Port=5432',
-    {$endif}
-      'postgres','postgres','postgresPassword');
-    VirtualTableExternalRegisterAll(aModel,aExternalDB);
-    aServer := TSQLRestServerDB.Create(aModel,SQLITE_MEMORY_DATABASE_NAME);
-    try // PostgreSQL uses one fork per connection -> better only two threads
-      aServer.AcquireExecutionMode[execORMGet] := amBackgroundThread;
-      aServer.AcquireExecutionMode[execORMWrite] := amBackgroundThread;
-    {$else}
     aServer := TSQLRestServerDB.Create(aModel,ChangeFileExt(paramstr(0),'.db'));
     try
       aServer.DB.Synchronous := smNormal;
       aServer.DB.LockingMode := lmExclusive;
-    {$endif}
       aServer.CreateMissingTables;
       aApplication := TBlogApplication.Create(aServer);
       try
@@ -88,9 +47,6 @@ begin
       end;
     finally
       aServer.Free;
-      {$ifdef USESYNDB}
-      aExternalDB.Free;
-      {$endif}
     end;
   finally
     aModel.Free;
