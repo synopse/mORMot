be64e5c9577624b0b8c754e7a23604a106735160
Synopse <webcontact01@synopse.info>
2015-03-03 13:55:01 +0100

    added mORMot4Android third-party demo source code - thanks Alf (Alfred) for sharing!

---
+++ SQLite3/Samples/ThirdPartyDemos/Alfred/mORMot4Android/gen/R.java.d
@@ -0,0 +1,8 @@
+C:\development\laz4android\examples\And\mOMRotOnAndroid\gen\com\kredix\R.java \
+ : C:\development\laz4android\examples\And\mOMRotOnAndroid\res\drawable-hdpi\icon.png \
+C:\development\laz4android\examples\And\mOMRotOnAndroid\res\drawable-ldpi\icon.png \
+C:\development\laz4android\examples\And\mOMRotOnAndroid\res\drawable-mdpi\icon.png \
+C:\development\laz4android\examples\And\mOMRotOnAndroid\res\drawable-xhdpi\icon.png \
+C:\development\laz4android\examples\And\mOMRotOnAndroid\res\drawable-xxhdpi\icon.png \
+C:\development\laz4android\examples\And\mOMRotOnAndroid\res\values\strings.xml \
+C:\development\laz4android\examples\And\mOMRotOnAndroid\bin\AndroidManifest.xml \
