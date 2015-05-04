de003c6408d44c20470ac9b688311c7134a9b405
Arnaud Bouchez <fakeaddress@synopse.info>
2015-05-04 14:58:25 +0200

    added FPCSQLITE3STATIC conditional to the FPC continuous integration build script

--- compilFPC.bat
+++ compilFPC.bat
@@ -16,7 +16,7 @@ del tempbuild\fpc\TestSQL3.* >nul
 
 cd \dev\lib\SQLite3
 
-c:\Development\FreePascal\fpc\bin\i386-win32\fpc.exe -B -MObjFPC -Scgi -CX -O2 -XX -ve -FiD:\Dev\Lib -FuD:\Dev\Lib -FuD:\Dev\Lib\SQLite3 -l -FlD:\Dev\Lib\fpc-win32 -FED:\Dev\Lib\tempbuild\fpc TestSQL3.dpr >D:\Dev\Lib\tempbuild\fpc\compilation
+c:\Development\FreePascal\fpc\bin\i386-win32\fpc.exe -B -MObjFPC -Scgi -CX -O2 -XX -ve -dFPCSQLITE3STATIC -FiD:\Dev\Lib -FuD:\Dev\Lib -FuD:\Dev\Lib\SQLite3 -l -FlD:\Dev\Lib\fpc-win32 -FED:\Dev\Lib\tempbuild\fpc TestSQL3.dpr >D:\Dev\Lib\tempbuild\fpc\compilation
 
 rem C:\development\fpc\bin\i386-win32\fpc.exe -B -MObjFPC -Scgi -CX -O2 -XX -ve -FiD:\Dev\Lib -FuD:\Dev\Lib -FuD:\Dev\Lib\SQLite3 -l -FlD:\Dev\Lib\fpc-win32 -FED:\Dev\Lib\tempbuild\fpc TestSQL3.dpr >D:\Dev\Lib\tempbuild\fpc\compilation
 
