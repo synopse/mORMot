e51d57f41c0943903d5161bedb97ca2148ff06f6
Arnaud Bouchez <fakeaddress@synopse.info>
2015-07-30 09:59:20 +0200

    include FPC units for DMustache githut commits

--- SQLite3/Documentation/SourceCodeRep/GitCommitDMustache.bat
+++ SQLite3/Documentation/SourceCodeRep/GitCommitDMustache.bat
@@ -16,6 +16,8 @@
 @xcopy %1\SynMustache.pas . /Y >nul
 @xcopy %1\Synopse.inc . /Y >nul
 @xcopy %1\SynopseCommit.inc . /Y >nul
+@xcopy %1\SynFPCLinux.pas . /Y >nul
+@xcopy %1\SynFPCTypInfo.pas . /Y >nul
 
 %3 add .
 %3 commit -a --file=%4
