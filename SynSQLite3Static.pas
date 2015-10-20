baa0753a6c5f784f9d2ea81827fb55b38a0b9bea
Arnaud Bouchez <fakeaddress@synopse.info>
2015-10-20 09:36:59 +0200

    updated SQLite3 engine to latest version 3.9.1 - also update .obj/.o corresponding binaries

--- SynSQLite3Static.pas
+++ SynSQLite3Static.pas
@@ -1,4 +1,4 @@
-/// SQLite3 3.9.0 Database engine - statically linked for Windows/Linux 32 bit
+/// SQLite3 3.9.1 Database engine - statically linked for Windows/Linux 32 bit
 // - this unit is a part of the freeware Synopse mORMot framework,
 // licensed under a MPL/GPL/LGPL tri-license; version 1.18
 unit SynSQLite3Static;
@@ -47,7 +47,7 @@ unit SynSQLite3Static;
 
 
 
-    Statically linked SQLite3 3.9.0 engine
+    Statically linked SQLite3 3.9.1 engine
    ****************************************
 
   To be declared in your project uses clause:  will fill SynSQlite3.sqlite3
@@ -74,7 +74,7 @@ unit SynSQLite3Static;
 
   Version 1.18
   - initial revision, extracted from SynSQLite3.pas unit
-  - updated SQLite3 engine to latest version 3.9.0
+  - updated SQLite3 engine to latest version 3.9.1
   - now all sqlite3_*() API calls are accessible via sqlite3.*()
   - our custom file encryption is now called via sqlite3.key() - i.e. official
     SQLite Encryption Extension (SEE) sqlite3_key() API
@@ -237,25 +237,6 @@ extern int unixWrite(
   int amt,
   sqlite3_int64 offset
 );
-
-
-Some notes for version 3.9.0 and JSON1 extension:
-
-  - fixed unknown isalnum/isdigit function calls:
-
-#define safe_isdigit(x) sqlite3Isdigit((unsigned char)(x))
-#define safe_isalnum(x) sqlite3Isalnum((unsigned char)(x))
-
-  - use of strtod() converted to:
-  
-    case JSON_REAL: {
-	  double r;
-	  if (sqlite3AtoF(pNode->u.zJContent, &r, sqlite3Strlen30(pNode->u.zJContent), SQLITE_UTF8)==0){
-	    r = 0;
-	  }
-      sqlite3_result_double(pCtx, r);
-      break;
-    }
 
 *)
 
