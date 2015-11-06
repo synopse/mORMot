da2bd0c28e3db14c4da40762667370fa36aaff74
Arnaud Bouchez <fakeaddress@synopse.info>
2015-11-06 20:02:53 +0100

    added TInterfacedObjectWithCustomCreate.RefCountUpdate low-level  wrapper method

--- SynCommons.pas
+++ SynCommons.pas
@@ -4806,6 +4806,13 @@ type
     // - this constructor does nothing, but is declared as virtual so that
     // inherited classes may safely override this default void implementation
     constructor Create; virtual;
+    /// used to mimic TInterfacedObject reference counting
+    // - Release=true will call TInterfacedObject._Release
+    // - Release=false will call TInterfacedObject._AddRef
+    // - could be used to emulate proper reference counting of the instance
+    // via interfaces variables, but still storing plain class instances
+    // (e.g. in a global list of instances)
+    procedure RefCountUpdate(Release: boolean); virtual;
   end;
 
   /// our own empowered TPersistent-like parent class
@@ -4816,15 +4823,18 @@ type
   // - for best performance, any type inheriting from this class will bypass
   // some regular steps: do not implement interfaces or use TMonitor with them!
   TSynPersistent = class(TObject)
-  private
-    procedure AssignError(Source: TSynPersistent);
   protected
+    // this default implementation will call AssignError()
     procedure AssignTo(Dest: TSynPersistent); virtual;
+    procedure AssignError(Source: TSynPersistent);
   public
     /// this virtual constructor will be called at instance creation
     // - this constructor does nothing, but is declared as virtual so that
     // inherited classes may safely override this default void implementation
     constructor Create; virtual;
+    /// allows to implement a TPersistent-like assignement mechanism
+    // - inherited class should override AssignTo() protected method
+    // to implement the proper assignment
     procedure Assign(Source: TSynPersistent); virtual;
     {$ifndef FPC_OR_PUREPASCAL}
     /// optimized x86 asm initialization code
@@ -39139,6 +39149,13 @@ constructor TInterfacedObjectWithCustomCreate.Create;
 begin // nothing to do by default - overridden constructor may add custom code
 end;
 
+procedure TInterfacedObjectWithCustomCreate.RefCountUpdate(Release: boolean);
+begin
+  if Release then
+    _Release else
+    _AddRef;
+end;
+
 
 { TSynLocker }
 
@@ -39213,7 +39230,9 @@ end;
 
 procedure TSynPersistent.Assign(Source: TSynPersistent);
 begin
-  if Source <> nil then Source.AssignTo(Self) else AssignError(nil);
+  if Source<>nil then
+    Source.AssignTo(Self) else
+    AssignError(nil);
 end;
 
 
