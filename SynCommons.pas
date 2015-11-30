391abf3a0321ba7a9382ccf1596534cf35889d58
Arnaud Bouchez <fakeaddress@synopse.info>
2015-11-30 10:21:05 +0100

    ensure FindFiles() will return 64 bit size information even on older versions of Delphi

--- SynCommons.pas
+++ SynCommons.pas
@@ -22457,7 +22457,11 @@ begin
         SetLength(result,n+1);
         with result[n] do begin
           Name := Dir+F.Name;
-          Size := F.Size; // may be limited to 32 bits on older Delphi
+          {$ifdef HASINLINE} // FPC or Delphi 2006+
+          Size := F.Size;
+          {$else} // F.Size was limited to 32 bits on older Delphi
+          Size := F.FindData.nFileSizeLow or Int64(F.FindData.nFileSizeHigh) shl 32;
+          {$endif}
           Attr := F.Attr;
           {$ifdef ISDELPHIXE2}
           TimeStamp := F.TimeStamp;
