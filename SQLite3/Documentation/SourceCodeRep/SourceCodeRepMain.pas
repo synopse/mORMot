54f994cc96666dff23a98bb7a15e2fbe782a5cd3
Arnaud Bouchez <fakeaddress@synopse.info>
2015-12-13 19:01:58 +0100

    fixed SourceCodeRep internal tool UI

--- SQLite3/Documentation/SourceCodeRep/SourceCodeRepMain.pas
+++ SQLite3/Documentation/SourceCodeRep/SourceCodeRepMain.pas
@@ -4,8 +4,17 @@ interface
 
 uses
   SynCommons,
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Clipbrd;
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  StdCtrls,
+  Clipbrd;
 
 const
   VERSION = '1.18';
@@ -45,6 +54,8 @@ type
     fDevPath: TFileName;
     fGitExe: TFileName;
     fGitRepository: TFileName;
+    function ExecAndWait(const Command, CurrentDir: TFileName; Visibility:
+      Word; Timeout: DWORD): integer;
     procedure ReadStatus;
   public
     { Public declarations }
@@ -59,37 +70,52 @@ implementation
 
 {$R Vista.res}
 
-function WinExecAndWait32(const Command,CurrentDir: TFileName;
-  Visibility: Word; Timeout : DWORD): integer;
+function TMainForm.ExecAndWait(const Command, CurrentDir: TFileName;
+  Visibility: Word; Timeout: DWORD): integer;
 var
   StartupInfo: TStartupInfo;
   ProcessInfo: TProcessInformation;
+  endTix: Int64;
 begin
-  FillChar(StartupInfo,SizeOf(TStartupInfo),0);
+  FillChar(StartupInfo, SizeOf(TStartupInfo), 0);
   with StartupInfo do begin
     cb := SizeOf(TStartupInfo);
     dwFlags := STARTF_USESHOWWINDOW or STARTF_FORCEONFEEDBACK;
     wShowWindow := visibility;
   end;
-  if CreateProcess(nil,pointer(Command),nil, nil, False, NORMAL_PRIORITY_CLASS,
-      nil, pointer(CurrentDir), StartupInfo, ProcessInfo) then
-    { timeout is in miliseconds or INFINITE if you want to wait forever }
-    result := Integer(WaitForSingleObject(ProcessInfo.hProcess, timeout)) else
+  if CreateProcess(nil, pointer(Command), nil, nil, False, NORMAL_PRIORITY_CLASS,
+    nil, pointer(CurrentDir), StartupInfo, ProcessInfo) then
+  try
+    Enabled := false;
+    Screen.Cursor := crHourGlass;
+    if Timeout = INFINITE then
+      TimeOut := 5 * 60 * 1000; // 5 minutes execution time seems enough
+    endTix := GetTickCount64 + Timeout;
+    repeat
+      Application.ProcessMessages;
+      result := WaitForSingleObject(ProcessInfo.hProcess, 100);
+    until (result = WAIT_OBJECT_0) or (GetTickCount64 > endTix);
+  finally
+    Enabled := true;
+    Screen.Cursor := crDefault;
+  end
+  else
     result := GetLastError;
 end;
 
 procedure TMainForm.ReadStatus;
 begin
-  WinExecAndWait32(fBatPath+'FossilStatus.bat "'+fBatPath+'status.txt"',fFossilRepository,SW_HIDE,10000);
-  mmoStatus.Text := StringFromFile(fBatPath+'status.txt');
+  ExecAndWait(fBatPath + 'FossilStatus.bat "' + fBatPath + 'status.txt"',
+    fFossilRepository, SW_HIDE, 10000);
+  mmoStatus.Text := StringFromFile(fBatPath + 'status.txt');
 end;
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
-  fBatPath := ExtractFilePath(paramstr(0));
-  if not FileExists(fBatPath+'FossilStatus.bat') then // from exe sub-folder?
+  fBatPath := ExeVersion.ProgramFilePath;
+  if not FileExists(fBatPath + 'FossilStatus.bat') then // from exe sub-folder?
     fBatPath := ExtractFilePath(ExcludeTrailingPathDelimiter(fBatPath));
-  if not FileExists(fBatPath+'FossilStatus.bat') then
+  if not FileExists(fBatPath + 'FossilStatus.bat') then
     ShowMessage('Missing .bat files');
   fDevPath := 'd:\dev\lib';
   fFossilRepository := 'c:\progs\fossil\lib';
@@ -98,7 +124,8 @@ begin
   if not DirectoryExists(fFossilRepository) then begin
     ShowMessage('Please set Fossil Repository Name');
     Close;
-  end else
+  end
+  else
     ReadStatus;
 end;
 
@@ -111,60 +138,65 @@ begin
 end;
 
 procedure TMainForm.btnFossilSynchClick(Sender: TObject);
-var Desc: string;
-    DescFile: TFileName;
-    VersionNumber: integer;
-    VersionText: RawUTF8;
+var
+  Desc: string;
+  DescFile: TFileName;
+  VersionNumber: integer;
+  VersionText: RawUTF8;
 begin
   Desc := trim(mmoDescription.Text);
-  if Desc='' then begin
+  if Desc = '' then begin
     ShowMessage('Missing description');
     mmoDescription.SetFocus;
     exit;
   end;
-  VersionText := UnQuoteSQLString(StringFromFile(fDevPath+'\SynopseCommit.inc'));
-  VersionText := GetCSVItem(pointer(VersionText),2,'.');
-  VersionNumber := GetCardinalDef(pointer(VersionText),255);
+  VersionText := UnQuoteSQLString(StringFromFile(fDevPath + '\SynopseCommit.inc'));
+  VersionText := GetCSVItem(pointer(VersionText), 2, '.');
+  VersionNumber := GetCardinalDef(pointer(VersionText), 255);
   inc(VersionNumber);
-  VersionText := ''''+VERSION+'.'+UInt32ToUtf8(VersionNumber)+''''#13#10;
-  FileFromString(VersionText,fDevPath+'\SynopseCommit.inc');
-  FileFromString(VersionText,fFossilRepository+'\SynopseCommit.inc');
-  DescFile := fBatPath+'desc.txt';
-  FileFromString('{'+IntToStr(VersionNumber)+'} '+Desc,DescFile);
-  WinExecAndWait32(fBatPath+'FossilCommit.bat "'+DescFile+'"',fFossilRepository,SW_SHOWNORMAL,INFINITE);
-  mmoDescription.SetFocus;
+  VersionText := '''' + VERSION + '.' + UInt32ToUtf8(VersionNumber) + ''''#13#10;
+  FileFromString(VersionText, fDevPath + '\SynopseCommit.inc');
+  FileFromString(VersionText, fFossilRepository + '\SynopseCommit.inc');
+  DescFile := fBatPath + 'desc.txt';
+  FileFromString('{' + IntToStr(VersionNumber) + '} ' + Desc, DescFile);
+  ExecAndWait(fBatPath + 'FossilCommit.bat "' + DescFile + '"',
+    fFossilRepository, SW_SHOWNORMAL, INFINITE);
+  btnRefreshStatus.Click;
 end;
 
 procedure TMainForm.btnGitSynchClick(Sender: TObject);
-var Desc,status: string;
-    DescFile, BatchFile: TFileName;
-    i: integer;
+var
+  Desc, status: string;
+  DescFile, BatchFile: TFileName;
+  i: integer;
 begin
   Desc := trim(mmoDescription.Text);
-  if Desc='' then begin
+  if Desc = '' then begin
     status := mmoStatus.Text;
-    i := pos('comment:',status);
-    if i>0 then begin
-      delete(status,1,i+8);
+    i := pos('comment:', status);
+    if i > 0 then begin
+      delete(status, 1, i + 8);
       with TStringList.Create do
       try
         Text := trim(status);
         status := Strings[0];
-        for i := 1 to Count-1 do
-          if copy(Strings[i],1,3)='   ' then
-            status := status+' '+trim(Strings[i]) else
+        for i := 1 to Count - 1 do
+          if copy(Strings[i], 1, 3) = '   ' then
+            status := status + ' ' + trim(Strings[i])
+          else
             break;
       finally
         Free;
       end;
-      i := pos('(user: ',status);
-      if i>0 then
-        SetLength(status,i-1);
-      i := pos('} ',status);
-      if (i>0) and (i<10) then
-        delete(status,1,i+1); // trim left '{256} '
+      i := pos('(user: ', status);
+      if i > 0 then
+        SetLength(status, i - 1);
+      i := pos('} ', status);
+      if (i > 0) and (i < 10) then
+        delete(status, 1, i + 1); // trim left '{256} '
       mmoDescription.Text := trim(status);
-    end else begin
+    end
+    else begin
       ShowMessage('Missing description');
       mmoDescription.SetFocus;
     end;
@@ -178,23 +210,24 @@ begin
     ShowMessage('Please set Git Repository Name');
     exit;
   end;
-  DescFile := fBatPath+'desc.txt';
-  FileFromString(Desc,DescFile);
-  if Sender=btnGitAll then
-    BatchFile := 'GitCommitAll.bat' else
-  if Sender=btnSynProject then
-    BatchFile := 'GitCommitSynProject.bat' else
-  if Sender=btnSynPdf then
-    BatchFile := 'GitCommitSynPdf.bat' else
-  if Sender=btnDMustache then
-    BatchFile := 'GitCommitDMustache.bat' else
-  if Sender=btnLVCL then
-    BatchFile := 'GitCommitLVCL.bat' else
+  DescFile := fBatPath + 'desc.txt';
+  FileFromString(Desc, DescFile);
+  if Sender = btnGitAll then
+    BatchFile := 'GitCommitAll.bat'
+  else if Sender = btnSynProject then
+    BatchFile := 'GitCommitSynProject.bat'
+  else if Sender = btnSynPdf then
+    BatchFile := 'GitCommitSynPdf.bat'
+  else if Sender = btnDMustache then
+    BatchFile := 'GitCommitDMustache.bat'
+  else if Sender = btnLVCL then
+    BatchFile := 'GitCommitLVCL.bat'
+  else
     BatchFile := 'GitCommit.bat';
-  WinExecAndWait32(format('%s%s "%s" "%s" "%s" "%s" "%s"',
-      [fBatPath,BatchFile,fFossilRepository,fGitRepository,fGitExe,DescFile,fDevPath]),
-     fGitRepository,SW_SHOWNORMAL,INFINITE);
-  mmoDescription.SetFocus;
+  ExecAndWait(format('%s%s "%s" "%s" "%s" "%s" "%s"', [fBatPath, BatchFile,
+    fFossilRepository, fGitRepository, fGitExe, DescFile, fDevPath]),
+    fGitRepository, SW_SHOWNORMAL, INFINITE);
+  mmoDescription.SetFocus; // ReadStatus not necessary if git only
 end;
 
 procedure TMainForm.btnRefreshStatusClick(Sender: TObject);
@@ -206,32 +239,34 @@ end;
 
 procedure TMainForm.btnGitShellClick(Sender: TObject);
 begin
-  WinExecAndWait32(format('%sGitShell.bat  "%s"',[fBatPath,ExtractFilePath(fGitExe)]),
-    fGitRepository,SW_SHOWNORMAL,INFINITE);
+  ExecAndWait(format('%sGitShell.bat  "%s"', [fBatPath, ExtractFilePath(fGitExe)]),
+    fGitRepository, SW_SHOWNORMAL, INFINITE);
 end;
 
 procedure TMainForm.btnFossilShellClick(Sender: TObject);
 begin
-  WinExecAndWait32('cmd.exe',fFossilRepository,SW_SHOWNORMAL,INFINITE);
+  ExecAndWait('cmd.exe', fFossilRepository, SW_SHOWNORMAL, INFINITE);
 end;
 
 procedure TMainForm.btnTestsClick(Sender: TObject);
 begin
-  WinExecAndWait32(fDevPath+'\compilpil.bat',fDevPath,SW_SHOWNORMAL,INFINITE);
+  ExecAndWait(fDevPath + '\compilpil.bat', fDevPath, SW_SHOWNORMAL, INFINITE);
 end;
 
 procedure TMainForm.btnCopyLinkClick(Sender: TObject);
-var i: integer;
-    status: string;
+var
+  i: integer;
+  status: string;
 begin
   status := mmoStatus.Lines.Text;
-  i := pos('checkout:',status);
-  if i<0 then
+  i := pos('checkout:', status);
+  if i < 0 then
     exit;
-  inc(i,10);
-  while (i<length(status)) and (status[i]<=' ') do inc(i);
-  Clipboard.AsText := 'http://synopse.info/fossil/info/'+copy(status,i,10);
+  inc(i, 10);
+  while (i < length(status)) and (status[i] <= ' ') do
+    inc(i);
+  Clipboard.AsText := 'http://synopse.info/fossil/info/' + copy(status, i, 10);
 end;
 
-
 end.
+
