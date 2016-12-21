d39d3b2f18b0ab916d6623db8686143ba2e5aa42
Arnaud Bouchez <fakeaddress@synopse.info>
2016-12-21 20:34:40 +0100

    speedup of TSQLRecordPropertiesMapping.ExternalToInternalIndex

--- SQLite3/mORMot.pas
+++ SQLite3/mORMot.pas
@@ -32090,8 +32090,8 @@ begin
   if IdemPropNameU(ExtFieldName,RowIDFieldName) then
     result := -1 else begin
     // search for customized field mapping
-    for result := 0 to high(fFieldNames) do
-      if IdemPropNameU(ExtFieldName,UnQuotedSQLSymbolName(fFieldNames[result])) then
+    for result := 0 to high(fExtFieldNamesUnQuotedSQL) do
+      if IdemPropNameU(ExtFieldName,fExtFieldNamesUnQuotedSQL[result]) then
         exit;
     result := -2; // indicates not found
   end;
