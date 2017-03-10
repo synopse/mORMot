7a485f9925f978f8433050e617e1522a72130020
Arnaud Bouchez <fakeaddress@synopse.info>
2017-03-10 11:32:15 +0100

    DDD's ToolsAdmin will create the save dialog component on need

--- SQLite3/DDD/tools/dddToolsAdminMain.pas
+++ SQLite3/DDD/tools/dddToolsAdminMain.pas
@@ -59,7 +59,6 @@ type
     SavePrefix: TFileName;
     OnAfterExecute: TOnExecute;
     OnAfterGetState: TNotifyEvent;
-    constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
     function Open(Definition: TDDDRestClientSettings; Model: TSQLModel = nil): boolean; virtual;
     procedure Show; virtual;
@@ -231,19 +230,6 @@ begin
   finally
     Screen.Cursor := crDefault;
   end;
-end;
-
-constructor TAdminControl.Create(AOwner: TComponent);
-begin
-  inherited;
-  fDlgSave := TSaveDialog.Create(AOwner);
-  fDlgSave.Options := [ofOverwritePrompt, ofHideReadOnly, ofPathMustExist,
-    ofEnableSizing];
-  fDlgSave.Filter :=
-    'JSON (human readable)|*.json|JSON (small)|*.json|CSV (text)|*.txt|Excel/Office (.ods)|*.ods|HTML|*.html';
-  fDlgSave.DefaultExt := '.html';
-  fDlgSave.FilterIndex := 5;
-  fDlgSave.InitialDir := GetShellFolderPath(CSIDL_DOCUMENTS);
 end;
 
 destructor TAdminControl.Destroy;
@@ -441,6 +427,16 @@ begin
   if (grid = nil) or (grid.RowCount = 0) then
     exit;
   if Fmt = expSaveGrid then begin
+    if fDlgSave = nil then begin
+      fDlgSave := TSaveDialog.Create(Owner);
+      fDlgSave.Options := [ofOverwritePrompt, ofHideReadOnly, ofPathMustExist,
+        ofEnableSizing];
+      fDlgSave.Filter :=
+        'JSON (human readable)|*.json|JSON (small)|*.json|CSV (text)|*.txt|Excel/Office (.ods)|*.ods|HTML|*.html';
+      fDlgSave.DefaultExt := '.html';
+      fDlgSave.FilterIndex := 5;
+      fDlgSave.InitialDir := GetShellFolderPath(CSIDL_DOCUMENTS);
+    end;
     if PropNameValid(pointer(db.GridLastTableName)) then
       name := db.GridLastTableName;
     fDlgSave.FileName := SysUtils.Trim(Format('%s %s %s',
