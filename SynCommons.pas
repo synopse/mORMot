a24bf7e391374bc8c1cd4ce3b6822a0da3f8e721
Arnaud Bouchez <fakeaddress@synopse.info>
2017-06-15 14:03:52 +0200

    fixed potential buffer overflow in TLecuyer.Seed

--- SynCommons.pas
+++ SynCommons.pas
@@ -34820,7 +34820,7 @@ begin
     crcblock(@crc.b,@time.b);
     crcblock(@crc.b,@ExeVersion.Hash.b);
     if entropy<>nil then
-      for i := 0 to entropylen do
+      for i := 0 to entropylen-1 do
         crc.b[i and 15] := crc.b[i and 15] xor entropy^[i];
     rs1 := rs1 xor crc.c0;
     rs2 := rs2 xor crc.c1;
@@ -37991,7 +37991,7 @@ asm  // faster version of _CopyRecord{dest, source, typeInfo: Pointer} by AB
         mov     ecx, [ebx].TTypeInfo.recSize
         test    ebp, ebp
         jz      @fullcopy
-        push    ecx                        // sizeof(record) on stack
+        push    ecx                          // sizeof(record) on stack
         add     ebx, offset TTypeInfo.ManagedFields[0] // ebx = first TFieldInfo
 @next:  mov     ecx, [ebx].TFieldInfo.&Offset
         mov     edx, [ebx].TFieldInfo.TypeInfo
@@ -38014,13 +38014,13 @@ asm  // faster version of _CopyRecord{dest, source, typeInfo: Pointer} by AB
         cmp     ecx, tkUString
         je      @UString
 {$else} cmp     ecx, tkDynArray
-        je      @DynArray
+        je      @dynaray
 {$endif}        ja      @err
         jmp     dword ptr[ecx * 4 + @tab - tkWString * 4]
 
-@Tab:   dd      @WString, @Variant, @ARRAY, @RECORD, @INTERFACE, @err
+@Tab:   dd      @WString, @variant, @array, @record, @interface, @err
 {$ifdef UNICODE}
-        dd      @DynArray
+        dd      @dynaray
 {$endif}
 @errv:  mov     al, reVarInvalidOp
         jmp     @err2
@@ -38031,7 +38031,7 @@ asm  // faster version of _CopyRecord{dest, source, typeInfo: Pointer} by AB
         pop     ebp
         jmp     System.Error
         nop // all functions below have esi=source edi=dest
-@Array: movzx   ecx, byte ptr[edx].TTypeInfo.NameLen
+@array: movzx   ecx, byte ptr[edx].TTypeInfo.NameLen
         push    dword ptr[edx + ecx].TTypeInfo.recSize
         push    dword ptr[edx + ecx].TTypeInfo.ManagedCount
         mov     ecx, dword ptr[edx + ecx].TTypeInfo.ManagedFields[0] // Fields[0].TypeInfo^
@@ -38040,7 +38040,7 @@ asm  // faster version of _CopyRecord{dest, source, typeInfo: Pointer} by AB
         call    System.@CopyArray
         pop     eax // restore sizeof(Array)
         jmp     @finish
-@Record:movzx   ecx, byte ptr[edx].TTypeInfo.NameLen
+@record:movzx   ecx, byte ptr[edx].TTypeInfo.NameLen
         mov     ecx, [edx + ecx].TTypeInfo.recSize
         push    ecx
         mov     ecx, edx
@@ -38051,7 +38051,7 @@ asm  // faster version of _CopyRecord{dest, source, typeInfo: Pointer} by AB
         nop
         nop
         nop
-@Variant:
+@variant:
 {$ifdef NOVARCOPYPROC}
         mov     edx, esi
         call    System.@VarCopy
@@ -38066,14 +38066,14 @@ asm  // faster version of _CopyRecord{dest, source, typeInfo: Pointer} by AB
         nop
         nop
 {$endif}
-@Interface:
+@interface:
         mov     edx, [esi]
         call    System.@IntfCopy
         jmp     @fin4
         nop
         nop
         nop
-@DynArray:
+@dynaray:
         mov     ecx, edx // ecx=TypeInfo
         mov     edx, [esi]
         call    System.@DynArrayAsg
@@ -40545,7 +40545,8 @@ begin
             ArrayTyp := TJSONCustomParserRTTI.TypeNameToSimpleRTTIType(
               @PByteArray(TypIdent)[1],len-9,ArrayTypIdent);
             if ArrayTyp=ptCustom then
-              raise ESynException.CreatEUTF8('%.Parse: unknown %',[self,TypIdent]);
+              raise ESynException.CreateUTF8('%.Parse: % is not a T*DynArray of a simple type',
+                [self,TypIdent]);
             Typ := ptArray;
           end;
           ExpectedEnd := eeNothing;
@@ -40612,7 +40613,7 @@ begin
           [self,fRoot.CustomTypeName]);
     {$else}
     raise ESynException.CreateUTF8('%.Create with no enhanced RTTI for %',
-      [self,TypeInfoToName(aRecordTypeInfo)]);
+      [self,PShortString(@PTypeInfo(aRecordTypeInfo).NameLen)^]);
     {$endif}
   end;
   fItems.Add(fRoot);
