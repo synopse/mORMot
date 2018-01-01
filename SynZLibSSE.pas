4dd062cceab6ac68335c12e83c2f4f53ca36553d
Arnaud Bouchez <fakeaddress@synopse.info>
2018-01-01 17:50:06 +0100

    happy new year with mORMot ! :)

--- SynZLibSSE.pas
+++ SynZLibSSE.pas
@@ -8,7 +8,7 @@ unit SynZLibSSE;
 
     This file is part of Synopse framework.
 
-    Synopse framework. Copyright (C) 2017 Arnaud Bouchez
+    Synopse framework. Copyright (C) 2018 Arnaud Bouchez
       Synopse Informatique - https://synopse.info
 
   *** BEGIN LICENSE BLOCK *****
@@ -27,7 +27,7 @@ unit SynZLibSSE;
 
   The Initial Developer of the Original Code is Arnaud Bouchez.
 
-  Portions created by the Initial Developer are Copyright (C) 2017
+  Portions created by the Initial Developer are Copyright (C) 2018
   the Initial Developer. All Rights Reserved.
 
   Contributor(s):
