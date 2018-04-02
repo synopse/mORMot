a39949e028125059028f16cfcda4ebccc866bf17
softpro <forvac@gmail.com>
2018-04-02 19:40:56 +0300

    Constants binding updated

--- SyNode/SyNodeBinding_const.pas
+++ SyNode/SyNodeBinding_const.pas
@@ -13,6 +13,61 @@ uses
 
 implementation
 
+uses
+  {$IFDEF MSWINDOWS}
+  Windows
+  {$ELSE}
+  BaseUnix
+  {$ENDIF}
+  ;
+
+{$IFDEF UNIX}
+const
+  O_DSYNC        = &0010000;
+  O_NOATIME      = &1000000;
+
+  // fs open() flags supported on other platforms
+  O_RANDOM       = 0;
+  O_SHORT_LIVED  = 0;
+  O_SEQUENTIAL   = 0;
+  O_TEMPORARY    = 0;
+
+  O_EXLOCK       = 0;
+  O_SYMLINK      = 0;
+{$ENDIF}
+{$IFDEF MSWINDOWS}
+const
+  F_OK = 0;
+  R_OK = 4;
+  W_OK = 2;
+  X_OK = 1;
+
+  // fs open() flags supported on Windows:
+  O_APPEND      = $0008;
+  O_CREAT       = $0100;
+  O_EXCL        = $0400;
+  O_RANDOM      = $0010;
+  O_RDONLY      = 0;
+  O_RDWR        = 2;
+  O_SEQUENTIAL  = $0020;
+  O_SHORT_LIVED = $1000;
+  O_TEMPORARY   = $0040;
+  O_TRUNC       = $0200;
+  O_WRONLY      = 1;
+
+  // fs open() flags supported on other platforms (or mapped on Windows):
+  O_DIRECT      = $02000000; // FILE_FLAG_NO_BUFFERING
+  O_DIRECTORY   = 0;
+  O_DSYNC       = $04000000; // FILE_FLAG_WRITE_THROUGH
+  O_EXLOCK      = $10000000; // EXCLUSIVE SHARING MODE
+  O_NOATIME     = 0;
+  O_NOCTTY      = 0;
+  O_NOFOLLOW    = 0;
+  O_NONBLOCK    = 0;
+  O_SYMLINK     = 0;
+  O_SYNC        = $08000000; // FILE_FLAG_WRITE_THROUGH
+{$ENDIF}
+
 function SyNodeBindingProc_consts(const aEngine: TSMEngine; const bindingNamespaceName: SynUnicode): jsval;
 var
   obj, obj_fs: PJSRootedObject;
@@ -25,19 +80,35 @@ begin
   obj := cx.NewRootedObject(cx.NewObject(nil));
   obj_fs := cx.NewRootedObject(cx.NewObject(nil));
   try
-   // file access modes
-   jsv.asInteger := fmOpenRead; obj_fs.ptr.DefineProperty(cx, 'O_RDONLY', jsv, attrs);
-   jsv.asInteger := fmOpenWrite; obj_fs.ptr.DefineProperty(cx, 'O_WRONLY', jsv, attrs);
-   jsv.asInteger := fmOpenReadWrite; obj_fs.ptr.DefineProperty(cx, 'O_RDWR', jsv, attrs);
+    jsv.asInteger := F_OK; obj_fs.ptr.DefineProperty(cx, 'F_OK', jsv, attrs);
+    jsv.asInteger := R_OK; obj_fs.ptr.DefineProperty(cx, 'R_OK', jsv, attrs);
+    jsv.asInteger := W_OK; obj_fs.ptr.DefineProperty(cx, 'W_OK', jsv, attrs);
+    jsv.asInteger := X_OK; obj_fs.ptr.DefineProperty(cx, 'X_OK', jsv, attrs);
 
-   jsv.asInteger := {$ifdef FPC}fmShareExclusive{$else}fmExclusive{$endif}; obj_fs.ptr.DefineProperty(cx, 'O_EXCL', jsv, attrs);
+    jsv.asInteger := O_APPEND; obj_fs.ptr.DefineProperty(cx, 'O_APPEND', jsv, attrs);
+    jsv.asInteger := O_CREAT; obj_fs.ptr.DefineProperty(cx, 'O_CREAT', jsv, attrs);
+    jsv.asInteger := O_EXCL; obj_fs.ptr.DefineProperty(cx, 'O_EXCL', jsv, attrs);
+    jsv.asInteger := O_RANDOM; obj_fs.ptr.DefineProperty(cx, 'O_RANDOM', jsv, attrs);
+    jsv.asInteger := O_RDONLY; obj_fs.ptr.DefineProperty(cx, 'O_RDONLY', jsv, attrs);
+    jsv.asInteger := O_RDWR; obj_fs.ptr.DefineProperty(cx, 'O_RDWR', jsv, attrs);
+    jsv.asInteger := O_SEQUENTIAL; obj_fs.ptr.DefineProperty(cx, 'O_SEQUENTIAL', jsv, attrs);
+    jsv.asInteger := O_SHORT_LIVED; obj_fs.ptr.DefineProperty(cx, 'O_SHORT_LIVED', jsv, attrs);
+    jsv.asInteger := O_TEMPORARY; obj_fs.ptr.DefineProperty(cx, '', jsv, attrs);
+    jsv.asInteger := O_TRUNC; obj_fs.ptr.DefineProperty(cx, 'O_TRUNC', jsv, attrs);
+    jsv.asInteger := O_WRONLY; obj_fs.ptr.DefineProperty(cx, 'O_WRONLY', jsv, attrs);
+
+    jsv.asInteger := O_DIRECT; obj_fs.ptr.DefineProperty(cx, 'O_DIRECT', jsv, attrs);
+    jsv.asInteger := O_DIRECTORY; obj_fs.ptr.DefineProperty(cx, 'O_DIRECTORY', jsv, attrs);
+    jsv.asInteger := O_DSYNC; obj_fs.ptr.DefineProperty(cx, 'O_DSYNC', jsv, attrs);
+    jsv.asInteger := O_EXLOCK; obj_fs.ptr.DefineProperty(cx, 'O_EXLOCK', jsv, attrs);
+    jsv.asInteger := O_NOATIME; obj_fs.ptr.DefineProperty(cx, 'O_NOATIME', jsv, attrs);
+    jsv.asInteger := O_NOCTTY; obj_fs.ptr.DefineProperty(cx, 'O_NOCTTY', jsv, attrs);
+    jsv.asInteger := O_NOFOLLOW; obj_fs.ptr.DefineProperty(cx, 'O_NOFOLLOW', jsv, attrs);
+    jsv.asInteger := O_NONBLOCK; obj_fs.ptr.DefineProperty(cx, 'O_NONBLOCK', jsv, attrs);
+    jsv.asInteger := O_SYMLINK; obj_fs.ptr.DefineProperty(cx, 'O_SYMLINK', jsv, attrs);
+    jsv.asInteger := O_SYNC; obj_fs.ptr.DefineProperty(cx, 'O_SYNC', jsv, attrs);
 
 // TODO define all other consts
-//   obj_fs.ptr.DefineProperty(cx, 'O_APPEND', , attrs)
-//   obj_fs.ptr.DefineProperty(cx, 'O_CREAT', , attrs)
-//   obj_fs.ptr.DefineProperty(cx, 'O_SYNC', , attrs)
-//   obj_fs.ptr.DefineProperty(cx, 'O_TRUNC', , attrs)
-//
 //
 //  NODE_DEFINE_CONSTANT(target, S_IFMT);
 //  NODE_DEFINE_CONSTANT(target, S_IFREG);
@@ -57,44 +128,6 @@ begin
 //
 //#ifdef S_IFSOCK
 //  NODE_DEFINE_CONSTANT(target, S_IFSOCK);
-//#endif
-//
-//
-//#ifdef O_NOCTTY
-//  NODE_DEFINE_CONSTANT(target, O_NOCTTY);
-//#endif
-//
-//#ifdef O_DIRECTORY
-//  NODE_DEFINE_CONSTANT(target, O_DIRECTORY);
-//#endif
-//
-//#ifdef O_EXCL
-//  NODE_DEFINE_CONSTANT(target, O_EXCL);
-//#endif
-//
-//#ifdef O_NOATIME
-//  NODE_DEFINE_CONSTANT(target, O_NOATIME);
-//#endif
-//
-//#ifdef O_NOFOLLOW
-//  NODE_DEFINE_CONSTANT(target, O_NOFOLLOW);
-//#endif
-//
-//#ifdef O_DSYNC
-//  NODE_DEFINE_CONSTANT(target, O_DSYNC);
-//#endif
-//
-//
-//#ifdef O_SYMLINK
-//  NODE_DEFINE_CONSTANT(target, O_SYMLINK);
-//#endif
-//
-//#ifdef O_DIRECT
-//  NODE_DEFINE_CONSTANT(target, O_DIRECT);
-//#endif
-//
-//#ifdef O_NONBLOCK
-//  NODE_DEFINE_CONSTANT(target, O_NONBLOCK);
 //#endif
 //
 //#ifdef S_IRWXU
@@ -145,21 +178,6 @@ begin
 //  NODE_DEFINE_CONSTANT(target, S_IXOTH);
 //#endif
 //
-//#ifdef F_OK
-//  NODE_DEFINE_CONSTANT(target, F_OK);
-//#endif
-//
-//#ifdef R_OK
-//  NODE_DEFINE_CONSTANT(target, R_OK);
-//#endif
-//
-//#ifdef W_OK
-//  NODE_DEFINE_CONSTANT(target, W_OK);
-//#endif
-//
-//#ifdef X_OK
-//  NODE_DEFINE_CONSTANT(target, X_OK);
-//#endif
     obj.ptr.DefineProperty(cx, 'fs', obj_fs.ptr.ToJSValue, attrs);
     Result := obj.ptr.ToJSValue;
   finally
