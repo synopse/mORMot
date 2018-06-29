bcc615ecdb47ea482aae3257c549f82cda1c0e4f
pavel.mash <7467039+pavelmash@users.noreply.github.com>
2018-06-29 14:33:21 +0300

    add some missing constants to SynZip

--- SynZip.pas
+++ SynZip.pas
@@ -495,12 +495,14 @@ type
 
 const
   ZLIB_VERSION = '1.2.3';
+  ZLIB_VERNUM = $1230;
 
   Z_NO_FLUSH = 0;
   Z_PARTIAL_FLUSH = 1;
   Z_SYNC_FLUSH = 2;
   Z_FULL_FLUSH = 3;
   Z_FINISH = 4;
+  Z_BLOCK = 5;
 
   Z_OK = 0;
   Z_STREAM_END = 1;
@@ -519,6 +521,8 @@ const
 
   Z_FILTERED = 1;
   Z_HUFFMAN_ONLY = 2;
+  Z_RLE = 3;
+  Z_FIXED = 4;
   Z_DEFAULT_STRATEGY = 0;
 
   Z_BINARY = 0;
