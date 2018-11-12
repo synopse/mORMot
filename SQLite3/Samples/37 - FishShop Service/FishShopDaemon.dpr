e37d6c64a6fd056e3adc7934c0f53802477a4a8c
Arnaud Bouchez <fakeaddress@synopse.info>
2018-11-12 10:22:10 +0100

    updated "37 - FishShop Service" with work done during EKON 22 workshop

--- SQLite3/Samples/37 - FishShop Service/FishShopDaemon.dpr
+++ SQLite3/Samples/37 - FishShop Service/FishShopDaemon.dpr
@@ -8,17 +8,24 @@ uses
   SynCommons,          // framework core
   SynLog,              // logging features
   mORMot,              // RESTful server & ORM
-  mORMotSQLite3,       // SQLite3 engine as ORM core
   SynSQLite3Static,    // staticaly linked SQLite3 engine
   mORMotHttpServer,    // HTTP server for RESTful server
   ServFishShopTypes,   // definitions shared by Server and Clients
   ServFishShopMain;    // the main implementation unit of our daemon
 
+var
+  daemon: TFishShopDaemon;
 begin
   // set logging abilities
   SQLite3Log.Family.Level := LOG_VERBOSE;
   //SQLite3Log.Family.EchoToConsole := LOG_VERBOSE;
   SQLite3Log.Family.PerThreadLog := ptIdentifiedInOnFile;
-  
+  // initialize the daemon
+  daemon := TFishShopDaemon.Create(TFishShopSettings, ExeVersion.ProgramFilePath, '', '');
+  try
+    daemon.CommandLine;
+  finally
+    daemon.Free;
+  end;
 end.
 
