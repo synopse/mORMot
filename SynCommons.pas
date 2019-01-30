6d27be53175e8c0734ad8d2d9535dfae0e52a857
Arnaud Bouchez <fakeaddress@synopse.info>
2019-01-30 19:47:25 +0100

    avoid some unexpected Delphi compiler hints

--- SynCommons.pas
+++ SynCommons.pas
@@ -56717,11 +56717,11 @@ begin
     TwoDigitToString({$ifdef CPU32}Int64Rec(Micro).Lo{$else}Micro{$endif} div 10,'ms',result) else
   if Micro<60000000 then
     TwoDigitToString({$ifdef CPU32}Int64Rec(Micro).Lo{$else}Micro{$endif} div 10000,'s',result) else
-  if Micro<3600000000 then
+  if Micro<QWord(3600000000) then
     TimeToString({$ifdef CPU32}Int64Rec(Micro).Lo{$else}Micro{$endif} div 1000000,'m',result) else
-  if Micro<86400000000*2 then
+  if Micro<QWord(86400000000*2) then
     TimeToString(Micro div 60000000,'h',result) else
-    FormatShort16('%d',[Micro div 86400000000],result)
+    FormatShort16('%d',[Micro div QWord(86400000000)],result)
 end;
 
 function IsInitializedCriticalSection(const CS: TRTLCriticalSection): Boolean;
