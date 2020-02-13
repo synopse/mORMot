db87be9ea75fc404fd5055b2d1f285385799b2cc
Arnaud Bouchez <fakeaddress@synopse.info>
2020-02-13 16:44:52 +0100

    try to circumvent missing files after rsynch to github repository

--- SQLite3/Documentation/SourceCodeRep/GitCommit.sh
+++ SQLite3/Documentation/SourceCodeRep/GitCommit.sh
@@ -17,6 +17,9 @@ $3 pull
 rsync -av --update --existing $1/* $2
 #rsync -av --update --existing $5/*.o* $2
 
+# wait a little, otherwise git fails to commit all modifications
+sleep 1
+
 $3 add .
 $3 commit -a --file=$4
 $3 push
